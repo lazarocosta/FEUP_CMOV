6d233301ca30d2c2453df334a050125af985b3fb
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-10-22 19:12:34 +0100

    Initial Android Studio project with 3 empty apps

---
+++ AndroidStudio/TicketAndPaymentSystem/customerapp/src/main/java/pt/up/fe/up201405729/cmov1/customerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.up.fe.up201405729.cmov1.customerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
