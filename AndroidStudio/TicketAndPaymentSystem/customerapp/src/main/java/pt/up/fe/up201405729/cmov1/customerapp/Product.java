f5d0370890b32e78d731917ee253c1ee643699de
lazarocosta <lazaroosta@hotmail.com>
2018-11-06 17:46:54 +0000

    add product

---
+++ AndroidStudio/TicketAndPaymentSystem/customerapp/src/main/java/pt/up/fe/up201405729/cmov1/customerapp/Product.java
@@ -0,0 +1,29 @@
+package pt.up.fe.up201405729.cmov1.customerapp;
+
+public class Product {
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Product(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+    public Product(String name, int price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
