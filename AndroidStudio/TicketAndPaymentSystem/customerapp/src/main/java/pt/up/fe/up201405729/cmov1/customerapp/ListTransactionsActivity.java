69a687bceff7915bde9dfdd204ce5e554b7e78f9
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-11-12 23:15:20 +0000

    Customer App: ListTransactionsActivity updating local storage

--- AndroidStudio/TicketAndPaymentSystem/customerapp/src/main/java/pt/up/fe/up201405729/cmov1/customerapp/ListTransactionsActivity.java
+++ AndroidStudio/TicketAndPaymentSystem/customerapp/src/main/java/pt/up/fe/up201405729/cmov1/customerapp/ListTransactionsActivity.java
@@ -17,15 +17,6 @@ import java.util.ArrayList;
 import pt.up.fe.up201405729.cmov1.restservices.RestServices;
 
 public class ListTransactionsActivity extends NavigableActivity {
-    private ArrayList<Ticket> listTickets;
-    private ArrayList<Voucher> listVouchers;
-    private ArrayList<Product> listProducts;
-    private TicketsRVAdapter ticketsRVAdapter;
-    private ProductsRVAdapter productsRVAdapter;
-    private VouchersRVAdapter vouchersRVAdapter;
-
-    private CustomerApp app;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +30,8 @@ public class ListTransactionsActivity extends NavigableActivity {
 
         SharedPreferences preferences = getSharedPreferences(CustomerApp.sharedPreferencesKeyName, Context.MODE_PRIVATE);
         String uuid = preferences.getString("uuid", null);
+
         final Context context = this;
-        this.app = (CustomerApp) getApplicationContext();
-
-
-
         JSONObject transactions = new JSONObject();
         try {
             transactions.put("userId", uuid);
@@ -52,18 +40,14 @@ public class ListTransactionsActivity extends NavigableActivity {
             Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
         }
         JSONObject response = RestServices.PUT("/listTransactionsUser", transactions);
-        listTickets = new ArrayList<>();
-        listProducts = new ArrayList<>();
-        listVouchers = new ArrayList<>();
-        System.out.println(response);
+        ArrayList<Ticket> listTickets = new ArrayList<>();
+        ArrayList<Product> listProducts = new ArrayList<>();
+        ArrayList<Voucher> listVouchers = new ArrayList<>();
         try {
             JSONObject data = response.getJSONObject("data");
             JSONArray vouchers = data.getJSONArray("vouchers");
             JSONArray tickets = data.getJSONArray("tickets");
             JSONArray products = data.getJSONArray("products");
-            System.out.println(products);
-            System.out.println(tickets);
-            System.out.println(vouchers);
 
             for (int i = 0; i < tickets.length(); i++) {
                 JSONObject jsonObject = tickets.getJSONObject(i);
@@ -75,7 +59,7 @@ public class ListTransactionsActivity extends NavigableActivity {
                 String state = jsonObject.getString("state");
                 listTickets.add(new Ticket(id, performanceId, name, date, roomPlace, state));
             }
-            System.out.println("aqui");
+
             for (int i = 0; i < products.length(); i++) {
                 JSONObject jsonObject = products.getJSONObject(i);
                 String name = jsonObject.getString("nameProduct");
@@ -84,16 +68,17 @@ public class ListTransactionsActivity extends NavigableActivity {
                 listProducts.add(new Product(name, price, quantity));
             }
 
-
             for (int i = 0; i < vouchers.length(); i++) {
                 JSONObject jsonObject = vouchers.getJSONObject(i);
                 String productCode = jsonObject.getString("productCode");
                 String id = jsonObject.getString("id");
                 String state = jsonObject.getString("state");
-                listVouchers.add(new Voucher(id,productCode,state));
+                listVouchers.add(new Voucher(id, productCode, state));
             }
-        }catch (JSONException e) {
-            System.out.println("erro");
+
+            FileManager.writeTickets(context, listTickets);
+            FileManager.writeVouchers(context, listVouchers);
+        } catch (JSONException e) {
             try {
                 Toast.makeText(context, response.getString("error"), Toast.LENGTH_LONG).show();
             } catch (JSONException e1) {
@@ -105,19 +90,19 @@ public class ListTransactionsActivity extends NavigableActivity {
         RecyclerView ticketsRV = findViewById(R.id.ticketsRecyclerView);
         GridLayoutManager gridLayoutManagerTickets = new GridLayoutManager(this, 1);
         ticketsRV.setLayoutManager(gridLayoutManagerTickets);
-        ticketsRVAdapter = new TicketsRVAdapter(listTickets);
+        TicketsRVAdapter ticketsRVAdapter = new TicketsRVAdapter(listTickets);
         ticketsRV.setAdapter(ticketsRVAdapter);
 
         RecyclerView productRV = findViewById(R.id.productsRecyclerView);
         GridLayoutManager gridLayoutManagerProduct = new GridLayoutManager(this, 1);
         productRV.setLayoutManager(gridLayoutManagerProduct);
-        productsRVAdapter = new ProductsRVAdapter(listProducts);
+        ProductsRVAdapter productsRVAdapter = new ProductsRVAdapter(listProducts);
         productRV.setAdapter(productsRVAdapter);
 
         RecyclerView voucherRV = findViewById(R.id.vouchersRecyclerView);
         GridLayoutManager gridLayoutManagerVouchers = new GridLayoutManager(this, 1);
         voucherRV.setLayoutManager(gridLayoutManagerVouchers);
-        vouchersRVAdapter = new VouchersRVAdapter(listVouchers);
+        VouchersRVAdapter vouchersRVAdapter = new VouchersRVAdapter(listVouchers);
         voucherRV.setAdapter(vouchersRVAdapter);
     }
 }
