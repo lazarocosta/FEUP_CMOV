4e4a80ab4db5cfedb6702b2c1ab76ce170cad0d5
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-12-03 23:26:24 +0000

    Project 2 recreated again
    This last version has been created with Visual Studio Community 2017 15.9.3.

--- Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis/MainPage.xaml.cs
+++ Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis/MainPage.xaml.cs
@@ -5,12 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace MyStocksAnalysis
-{
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
+namespace MyStocksAnalysis {
+    public partial class MainPage : ContentPage {
+        public MainPage() {
             InitializeComponent();
         }
     }
