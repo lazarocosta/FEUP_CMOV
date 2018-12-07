e328cd4ad234eef9c555aca0820945cf842770c7
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-12-07 20:29:52 +0000

    Checkout's logic ready and UI partially ready

--- Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis/Checkout.xaml.cs
+++ Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis/Checkout.xaml.cs
@@ -6,32 +6,58 @@ using System;
 using System.Collections.Generic;
 using System.Collections;
 
-namespace MyStocksAnalysis
-{
-    public partial class Checkout : ContentPage{
+namespace MyStocksAnalysis {
+    public partial class Checkout : ContentPage {
         private List<string> companies;
-        private double days = 7;
+        private Label label;
+        private double days;
 
-    
-        public Checkout(List<string> companies){
+        public Checkout(List<string> companies) {
+            if (companies.Count < 1 || companies.Count > 2)
+                throw new ArgumentException("Invalid number of companies.");
             this.companies = companies;
+            InitializeContent();
             InitializeComponent();
+            Slider_ValueChanged(this, new ValueChangedEventArgs(0, 0));
         }
 
-        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
+        private void InitializeContent() {
+            List<Cell> cells = new List<Cell>();
+            foreach (string c in this.companies) {
+                TextCell textCell = new TextCell { Text = c };
+                cells.Add(textCell);
+            }
+            TableSection tableSection = new TableSection { cells };
+            TableView tableView = new TableView {
+                Root = new TableRoot("Companies") { tableSection },
+                Intent = TableIntent.Data
+            };
+            label = new Label();
+            Slider slider = new Slider(min: 0, max: 23, val: 0);
+            slider.ValueChanged += Slider_ValueChanged;
+            Button button = new Button {
+                Text = "Show graph"
+            };
+            button.Clicked += Button_Clicked;
+            Content = new StackLayout {
+                Children = {
+                    tableView,
+                    label,
+                    slider,
+                    button
+                }
+            };
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e) {
             double value = e.NewValue + 7;
             this.days = Math.Floor(value);
-            label.Text = "Number of days " + this.days ;
-            Console.WriteLine("show days" + this.days);
+            label.Text = "Number of days: " + this.days;
         }
         // to go back one step on the navigation stack
         // Navigation.PopAsync();
 
-        private void ShowGraph_Clicked(object sender, EventArgs e)
-        {
-            Console.WriteLine("show days__" + this.days);
-
+        private void Button_Clicked(object sender, EventArgs e) {
             Navigation.PushAsync(new Graph(this.companies, this.days));
         }
     }
