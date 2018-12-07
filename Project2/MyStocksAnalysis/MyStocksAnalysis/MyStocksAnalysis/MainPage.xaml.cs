abab6648950ae294c6a62147387a5b9b58b670ce
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-12-07 19:55:35 +0000

    MainPage's logic and UI fully working

--- Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis/MainPage.xaml.cs
+++ Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis/MainPage.xaml.cs
@@ -8,42 +8,70 @@ using System.Collections;
 
 namespace MyStocksAnalysis {
     public partial class MainPage : ContentPage {
-        public List<string> itemsSelected;
+        private static List<string> companies = new List<string>() {
+            "AMD",
+            "Apple",
+            "Facebook",
+            "Google",
+            "Hewlett Packard",
+            "IBM",
+            "Intel",
+            "Microsoft",
+            "Oracle",
+            "Twitter"
+        };
+        private List<string> itemsSelected;
+        private Button button;
+
         public MainPage() {
             this.itemsSelected = new List<string>();
+            InitializeContent();
             InitializeComponent();
-            CompaniesSource.ItemsSource = new List<string>
-            {
-                "AMD",
-                "Apple",
-                "Facebook",
-                "Google",
-                "Hewlett Packard",
-                "IBM",
-                "Intel",
-                "Microsoft",
-                "Oracle",
-                "Twitter"
+        }
+
+        private void InitializeContent() {
+            List<Cell> cells = new List<Cell>();
+            foreach (string c in companies) {
+                SwitchCell switchCell = new SwitchCell { Text = c };
+                switchCell.OnChanged += SwitchCellOnChangedHandler;
+                cells.Add(switchCell);
+            }
+            TableSection tableSection = new TableSection { cells };
+            TableView tableView = new TableView {
+                Root = new TableRoot("Companies") { tableSection },
+                Intent = TableIntent.Form
             };
-
-            CompaniesSource.ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
-            {
-                var item = e.SelectedItem;
-                string itemString = item.ToString();
-
-                if(this.itemsSelected.Contains(itemString))
-                    this.itemsSelected.Remove(itemString);
-                else
-                    this.itemsSelected.Add(itemString);
-
-                DisplayAlert("ItemSelected", this.itemsSelected.Contains(itemString).ToString(), "OK");
+            button = new Button {
+                Text = "Next",
+                IsEnabled = false
+            };
+            button.Clicked += Button_Clicked;
+            Content = new StackLayout {
+                Children = {
+                    tableView,
+                    button
+                }
             };
         }
 
-        private void ShowGraph_Clicked(object sender, EventArgs e)
-        {
+        private void SwitchCellOnChangedHandler(object sender, ToggledEventArgs e) {
+            SwitchCell switchCell = (SwitchCell)sender;
+            string itemString = switchCell.Text;
+            if (e.Value) {
+                if (this.itemsSelected.Count >= 2)
+                    DisplayAlert("Too many options", "You can only select one or two options.", "OK");
+                this.itemsSelected.Add(itemString);
+            }
+            else
+                this.itemsSelected.Remove(itemString);
+            if (this.itemsSelected.Count >= 1 && this.itemsSelected.Count <= 2)
+                button.IsEnabled = true;
+            else
+                button.IsEnabled = false;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) {
             Navigation.PushAsync(new Checkout(this.itemsSelected));
         }
     }
-
 }
