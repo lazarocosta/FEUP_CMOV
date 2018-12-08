1adbbe956c48de6be90bdb99dd39188500709c7e
lazarocosta <lazaroosta@hotmail.com>
2018-12-08 13:28:07 +0000

    add file

---
+++ Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis/Response.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyStocksAnalysis
+{
+    public class Response
+    {
+        public IList<string> Status { get; set; }
+        public IList<Result> Results { get; set; }
+
+        public class Result
+        {
+            public string symbol { get; set; }
+            public DateTime timestamp { get; set; }
+
+            public DateTime tradingDay { get; set; }
+            public double open { get; set; }
+            public double hight { get; set; }
+            public double low { get; set; }
+            public double close{ get; set; }
+            public int volume { get; set; }
+            public double openInterest { get; set; }
+        }
+    }
+}
