0b894679ac61895a215da3d504bd34bd398ce60c
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2018-12-14 12:43:05 +0000

    The presented names of the applications have been updated

--- Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis.Android/MainActivity.cs
+++ Project2/MyStocksAnalysis/MyStocksAnalysis/MyStocksAnalysis.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MyStocksAnalysis.Droid
 {
-    [Activity(Label = "MyStocksAnalysis", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "My Stocks Analysis", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
